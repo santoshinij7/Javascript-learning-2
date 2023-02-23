ae11308da0b0c2158f747c1af6d4ae788bba32f7
santoshinij7 <santoshinij7@gmail.com>
2023-02-23 21:13:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -92,3 +92,30 @@ const yearsUntilRetirement = function (birthYear,firstName){
 console.log(yearsUntilRetirement(1991, 'lucky'));
 console.log(yearsUntilRetirement(1950, 'lucy'));
 */
+//CODING CHALLENGE
+//TEST - 1:
+const calcAverage = (a,b,c) =>(a+b+c)/3;
+console.log(calcAverage(3,4,5));
+
+let ScoreDolphins = calcAverage(44,23,71);
+let ScoreKobals = calcAverage(65,54,49);
+console.log(ScoreDolphins,ScoreKobals);
+
+const checkWinner = function(avgDolphins, avgKobals){
+    if(avgDolphins>= 2 * avgKobals){
+        console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKobals})`);
+    } else if(avgKobals >= 2* avgDolphins){
+        console.log(`kobals win 🏆 (${avgKobals} vs. ${avgDolphins})`);
+    }else{
+        console.log('No team wins...')
+    }
+}
+checkWinner(ScoreDolphins, ScoreKobals);
+checkWinner(576,111); 
+
+//TEST 2:
+ScoreDolphins = calcAverage(85,54,41);
+ScoreKobals = calcAverage(23,34,27);
+console.log(ScoreDolphins,ScoreKobals);
+checkWinner(ScoreDolphins, ScoreKobals);
+
